c06be31e3c645bcaecbddc1c05f1dab284625bd3
Chi-En Wu <jason2506.mail@gmail.com>
2014-02-03 01:32:02 +0800

    Remove jQuery from options page

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -1,110 +1,107 @@
 (function() {
+    const ID_PTN = /^[a-zA-Z0-9\-_]{11}$/;
+    var bgPage          = chrome.extension.getBackgroundPage(),
+        player          = bgPage.player,
+        forEach         = Array.prototype.forEach,
+        sidebarItems    = document.querySelectorAll('#sidebar li'),
+        modeSelectors   = document.querySelectorAll('input[name="playmode"]'),
+        importMsg       = document.querySelector('#import-export-page .msg'),
+        importTextarea  = document.getElementById('import'),
+        importButton    = document.getElementById('submit-import'),
+        exportTextarea  = document.getElementById('export'),
+        exportButton    = document.getElementById('refresh-export');
 
-var bgPage = chrome.extension.getBackgroundPage();
-var player = bgPage.player;
-var componments = {
-    sidebarItems:       $('#sidebar li'),
-
-    playmodeSelectors:  $('input[name=playmode]:radio'),
-
-    importResultMsg:    $('#import-export-page .msg'),
-    importTextarea:     $('#import'),
-    importButton:       $('#submit-import'),
-    exportTextarea:     $('#export'),
-    exportButton:       $('#refresh-export')
-};
-
-/*
- * Sidebar Control
- */
-componments.sidebarItems.click(function() {
-    $('.current').removeClass('current');
-
-    var targetId = $(this).attr('rel');
-    $(this).addClass('current');
-    $('#' + targetId).addClass('current');
-});
-
-$(componments.sidebarItems[0]).click();
-
-/*
- * Player Options
- */
-componments.playmodeSelectors.each(function() {
-    var mode = player.playmode();
-    if ($(this).val() === mode)
-        $(this).attr('checked', 'true');
-});
-
-componments.playmodeSelectors.click(function() {
-    var mode = $(this).val();
-    player.playmode(mode);
-});
-
-/*
- * Export / Import
- */
-exportPlaylist();
-componments.importButton.click(importPlaylist);
-componments.exportButton.click(exportPlaylist);
-
-function importPlaylist() {
-    var msg = componments.importResultMsg;
-    msg.removeClass('success error');
-
-    var json = componments.importTextarea.val();
-    var result;
-    try {
-        result = JSON.parse(json);
-    }
-    catch (error) {
-        msg.addClass('error')
-            .text('The imported text is not a valid JSON string.');
-        return;
+    function showMsg(type, msg) {
+        importMsg.classList.add(type);
+        importMsg.textContent = msg;
     }
 
-    if (!$.isArray(result)) {
-        msg.addClass('error')
-            .text('The imported object is not a array of video id-title pair.');
-        return;
-    }
+    function importPlaylist() {
+        var json = importTextarea.value,
+            playlist,
+            index,
+            length,
+            id,
+            title;
 
-    const idPattern = /^[a-zA-Z0-9\-_]{11}$/;
-    var playlist = [];
-    for (var index = 0; index < result.length; index++)
-    {
-        var id = result[index].id;
-        var title = result[index].title;
-
-        if (typeof id !== 'string') {
-            msg.addClass('error')
-                .text('The video id "' + id + '" is not a string.');
-            return;
+        importMsg.classList.remove('success');
+        importMsg.classList.remove('error');
+        try {
+            playlist = JSON.parse(json);
         }
-        else if (!idPattern.test(id)) {
-            msg.addClass('error')
-                .text('"' + id + '" is not a valid video id.');
-            return;
-        }
-        else if (typeof title !== 'string' || title.length === 0) {
-            msg.addClass('error')
-                .text('The video title is not valid.');
+        catch (error) {
+            showMsg('error', 'Imported text is not a valid JSON string.');
             return;
         }
 
-        playlist.push({
-            id: result[index].id,
-            title: result[index].title
-        });
+        if (!Array.isArray(playlist)) {
+            showMsg('error', 'Imported object must be a array of (id, title) pair.');
+            return;
+        }
+
+        length = playlist.length;
+        for (index ; index < length; index++)
+        {
+            id = playlist[index].id;
+            title = playlist[index].title;
+            if (typeof id !== 'string') {
+                showMsg('error', 'Video id "' + id + '" is not a string.');
+                return;
+            }
+            else if (!ID_PTN.test(id)) {
+                showMsg('error', '"' + id + '" is not a valid video id.');
+                return;
+            }
+            else if (typeof title !== 'string' || title.length === 0) {
+                showMsg('error', 'Video title is not valid.');
+                return;
+            }
+        }
+
+        player.import(playlist);
+        showMsg('success', 'Import successful.');
     }
 
-    player.import(playlist);
-    msg.addClass('success').text('Import successful.');
-}
+    function exportPlaylist() {
+        var json = JSON.stringify(player.playlist);
+        exportTextarea.value = json;
+    }
 
-function exportPlaylist() {
-    var json = JSON.stringify(player.playlist);
-    componments.exportTextarea.val(json);
-}
+    function onSidebarItemClicked() {
+        var targetId = this.getAttribute('rel'),
+            currentSidebarItem = document.querySelector('li.current'),
+            currentPage = document.querySelector('div.current');
 
+        if (currentSidebarItem !== null) {
+            currentSidebarItem.classList.remove('current');
+            currentPage.classList.remove('current');
+        }
+
+        document.getElementById(targetId).classList.add('current');
+        this.classList.add('current');
+    }
+
+    function onPlayModeSelectorClicked() {
+        player.playmode(this.value);
+    }
+
+    // Sidebar Control
+    forEach.call(sidebarItems, function(item) {
+        item.addEventListener('click', onSidebarItemClicked);
+    });
+
+    sidebarItems[0].click();
+
+    // Player Options
+    forEach.call(modeSelectors, function(selector) {
+        selector.addEventListener('click', onPlayModeSelectorClicked);
+        if (selector.value === player.playmode()) {
+            selector.setAttribute('checked', 'true');
+        }
+    });
+
+    // Export / Import
+    exportPlaylist();
+    importButton.addEventListener('click', importPlaylist);
+    exportButton.addEventListener('click', exportPlaylist);
 })();
